f8c2ebaef9ec823228115f0caf290f841165ed91
Vincent Grovestine <vincent.grovestine@acadiau.ca>
2022-06-21 17:06:56 -0300

    Remove placeholder config defaults.

--- alma-open-hours.config.php
+++ alma-open-hours.config.php
@@ -24,9 +24,9 @@ this program.  If not, see <https://www.gnu.org/licenses/>.
 
 // Alma config
 // Refer to https://developers.exlibrisgroup.com/alma/apis/docs/conf/R0VUIC9hbG1hd3MvdjEvY29uZi9saWJyYXJpZXMve2xpYnJhcnlDb2RlfS9vcGVuLWhvdXJz/
-$ALMA_API_BASEURL = 'https://api-ca.hosted.exlibrisgroup.com/almaws/v1/conf/libraries/';
-$ALMA_API_KEY = 'My_Alma_API_Key';
-$ALMA_API_LIBRARY_ID = 'My_Library_ID'; 
+$ALMA_API_BASEURL = '';
+$ALMA_API_KEY = '';
+$ALMA_API_LIBRARY_ID = ''; 
 $ALMA_API_QUERY_DAYS = 28; // API querying supports between 1 and 28 days
 $ALMA_API_QUERY_DAYS_MULTIPLE = 1; // Retrieve a multiple of API query days between 1 and 13
 
